a9d5adfb1b233eb676cd3c534192dad45664689d
mich-grab <103949950+mich-grab@users.noreply.github.com>
2022-06-28 18:03:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,32 @@ draggables.forEach(draggable => {
 })
 
 nests.forEach(nest => {
-    nest.addEventListener('dragover', () => {
-        console.log('dragover')
+    nest.addEventListener('dragover', e => {
+        e.preventDefault()
+        const afterElement = getDragAfterElement(nest, e.clientX)
+        const draggable = document.querySelector('.dragging')
+
+        if (afterElement == null) {
+            nest.appendChild(draggable)
+        } else {
+            nest.insertBefore(draggable, afterElement)
+        }
+
+
     })
 })
+
+function getDragAfterElement(nest, x) {
+    const draggableElements = [...nest.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect()
+        const offset = x - box.left - box.width / 2
+
+        if (offset < 0 && offset > closest.offset) {
+            return { offset: offset, element: child }
+        } else {
+            return closest
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+}
