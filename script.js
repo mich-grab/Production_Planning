2acb094b0e4418d9e07fc2377a024d3795c7e23d
mich-grab <103949950+mich-grab@users.noreply.github.com>
2022-07-18 21:44:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import toolDB from './database.json' assert {type: 'json'}
 
-let draggables = document.getElementsByName('guide')
+/* const draggables = document.querySelectorAll('.draggable') */
+let draggables = document.getElementsByClassName('draggable');
 const nests = document.querySelectorAll('.planner__right__nest')
 const toolList = document.querySelector('.toolList')
 
@@ -35,6 +36,9 @@ console.log(toolList)
 console.log(newArray)
  */
 
+console.log(parent)
+
+
 
 tools.forEach(tool => {
 
@@ -58,9 +62,7 @@ function fillToolInfo(toolFullName) {
             info_toolNumbers.innerText = toolDB.toolList[i].toolNumber
             let guides_list = ''
             for (let j = 0; j < toolDB.toolList[i].guideList.length; j++) {
-                console.log(guides_list)
                 guides_list += toolDB.toolList[i].guideList[j].guide
-                console.log(guides_list)
             }
             info_toolGuidelist.innerText = guides_list
 
@@ -79,16 +81,14 @@ function createGuides(number) {
         newDiv.classList.add('process', 'draggable', 'red')
         newDiv.setAttribute('draggable', true)
         newDiv.name = 'guide'
+        newDiv.style.backgroundColor = toolDB.toolList[number].toolcolor
         info_plannerRightBottom.appendChild(newDiv)
-        draggables.append(newDiv)
     }
-    /* console.log(draggables) */
-
 }
 
 
 
-/* console.log(draggables) */
+
 
 
 draggables.forEach(draggable => {
@@ -99,30 +99,23 @@ draggables.forEach(draggable => {
         draggable.classList.remove('dragging')
     })
 })
-
 nests.forEach(nest => {
     nest.addEventListener('dragover', e => {
         e.preventDefault()
         const afterElement = getDragAfterElement(nest, e.clientX)
         const draggable = document.querySelector('.dragging')
-
         if (afterElement == null) {
             nest.appendChild(draggable)
         } else {
             nest.insertBefore(draggable, afterElement)
         }
-
-
     })
 })
-
 function getDragAfterElement(nest, x) {
     const draggableElements = [...nest.querySelectorAll('.draggable:not(.dragging)')]
-
     return draggableElements.reduce((closest, child) => {
         const box = child.getBoundingClientRect()
         const offset = x - box.left - box.width / 2
-
         if (offset < 0 && offset > closest.offset) {
             return { offset: offset, element: child }
         } else {
